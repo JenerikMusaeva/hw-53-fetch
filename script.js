0eddfc83022874de9c26a9869f2977d9292ee2e8
JenerikMusaeva <74776910+JenerikMusaeva@users.noreply.github.com>
2021-02-23 20:15:02 +0600

    optimization code without then

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-const userUrl = 'https://jsonplaceholder.typicode.com/users'
 const contactList = document.querySelector('#contact-list')
 
-function renderUsers(list) {
-  list.forEach(item => {
+async function getUsers() {
+  let response = await fetch('https://jsonplaceholder.typicode.com/users')
+  let users = await response.json()
+  return users
+}
+
+async function renderUsers() {
+  let users = await getUsers()
+  users.forEach(item => {
     let user = document.createElement('tr')
     user.innerHTML = `
       <td>${item.name}</td>
       <td>${item.username}</td>
       <td>${item.phone}</td>
+      <td>${item.email}</td>
     `
     contactList.append(user)
   })
 }
 
-async function getUsers() {
-  let response = await fetch(userUrl)
-  let users = await response.json()
-  return users
-}
-
-getUsers().then((data) => {
-  renderUsers(data)
-})
+renderUsers()
